9adb186092e6b9ce02fe28fd31930a901d15df70
Mehmet Uyanik <Memuya@users.noreply.github.com>
2014-11-30 16:13:35 +1100

    Create RPS.php

---
+++ classes/RPS.php
@@ -0,0 +1,51 @@
+<?php
+class RPS {
+	private $player, $ai, $selections, $results;
+
+	public function __construct() {
+		$this->selections = [
+			'rock' => 'sissors',
+			'paper' => 'rock',
+			'sissors' => 'paper'
+		];
+	}
+
+	public function battle() {
+		$this->setAI();
+		foreach($this->selections as $key => $x) {
+			//player wins
+			if($this->player == $key && $this->ai == $x) $this->results = 'YOU WIN<br>'.$key.' beats '.$x;
+			//ai wins
+			else if($this->ai == $key && $this->player == $x) $this->results = 'YOU LOSE<br>'.$key.' beats '.$x;
+			//draw
+			else if($this->player == $key && $this->ai == $key) $this->results = 'DRAW<br>'.$this->player.' and '.$this->ai.' are the same';
+		}
+	}
+
+	public function results() {
+		return $this->results;
+	}
+
+	public function setPlayer($x) {
+		//if player selection is not in array, default to 'rock'
+		if(in_array($x, $this->selections))
+			$this->player = $x;
+		else
+			$this->player = 'rock';
+
+		//un-comment to randomize player selection
+		//$this->player = array_rand($this->selections);
+	}
+
+	public function setAI() {
+		$this->ai = array_rand($this->selections);
+	}
+
+	public function getPlayer() {
+		return $this->player;
+	}
+
+	public function getAI() {
+		return $this->ai;
+	}
+}
